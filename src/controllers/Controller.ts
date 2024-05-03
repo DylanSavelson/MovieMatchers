e3de3d296879f40ff6b30a4c8499aea005c48c22
DylanSavelson <dylannsavelson@gmail.com>
2024-05-03 19:26:00 +0000

    Added login

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -1,29 +1,141 @@
 import postgres from "postgres";
+import Router from "../router/Router";
 import Request from "../router/Request";
 import Response, { StatusCode } from "../router/Response";
-import Router from "../router/Router";
+import User, { InvalidCredentialsError, UserProps } from "../models/User";
+import SessionManager from "../auth/SessionManager";
+import Cookie from "../auth/Cookie";
+import Session from "../auth/Session"
+import { spec } from "node:test/reporters";
+
 
 /**
  * Controller for handling Todo CRUD operations.
  * Routes are registered in the `registerRoutes` method.
  * Each method should be called when a request is made to the corresponding route.
  */
-export default class Controller {
+export default class AuthController {
 	private sql: postgres.Sql<any>;
-
+	private sessionManager: SessionManager;
 	constructor(sql: postgres.Sql<any>) {
 		this.sql = sql;
+		this.sessionManager = SessionManager.getInstance();
 	}
 
-	/**
-	 * To register a route, call the corresponding method on
-	 * the router instance based on the HTTP method of the route.
-	 *
-	 * @param router Router instance to register routes on.
-	 *
-	 * @example router.get("/todos", this.getTodoList);
-	 */
 	registerRoutes(router: Router) {
-		// Any routes that include a `:id` parameter should be registered last.
+		router.get("/register", this.getRegistrationForm);
+		router.get("/login", this.getLoginForm);
+		router.post("/login", this.login);
+		router.get("/logout", this.logout);
 	}
+
+	getRegistrationForm = async (req: Request, res: Response) => {
+		const session = req.getSession();
+		res.setCookie( 
+			session.cookie
+		  );
+		await res.send({
+			statusCode: StatusCode.OK,
+			message: "New sign up form",
+			payload: {
+				isDarkMode: req.findCookie("theme")?.value === "dark",
+				error: req.getSearchParams().get("error"),
+				sessionCookie: session.get("userId") ? true : false,
+				userId: session.get("userId")
+			},
+			template: "RegistrationFormView",
+		});
+	};
+
+
+	getLoginForm = async (req: Request, res: Response) => {
+		const session = req.getSession();
+		res.setCookie( 
+			session.cookie
+		  );
+		let specifiedError = req.getSearchParams().get("error");
+		if (specifiedError)
+		{
+			specifiedError = specifiedError + "."
+		}
+		await res.send({
+			statusCode: StatusCode.OK,
+			message: "Login Form",
+			payload: {
+				error: specifiedError,
+				rememberedEmail: req.findCookie("email")?.value,
+				sessionCookie: session.get("userId") ? true : false,
+				userId: session.get("userId")
+			},
+			template: "LoginFormView",
+		});
+	};
+
+	login = async (req: Request, res: Response) => {
+		const password = req.body.password
+		const email = req.body.email
+		const remember = req.body.remember
+		try 
+		{
+			const user = await User.login(this.sql, email, password);
+
+			const session = req.getSession();
+			session.set("userId", user.props.id);
+
+			if (remember)
+			{
+				res.setCookie(
+					new Cookie(
+						"email",
+						email
+					)
+				)
+			}
+			await res.send({
+				statusCode: StatusCode.OK,
+				message: "Successful Login",
+				redirect: "/todos",
+			});
+		} catch (error) {
+			if (!email)
+			{
+				await res.send({
+					statusCode: StatusCode.BadRequest,
+					message: "someting wrong",
+					redirect: "/login?error=Email is required",
+				});
+				
+			}
+
+			else if(this.isInvalidCredentialsError(error))
+			{
+				let errorMessage = error.message.slice(0, -1);
+				await res.send({
+					statusCode: StatusCode.BadRequest,
+					message: "someting wrong",
+					redirect: "/login?error="+ errorMessage,
+				});
+			}
+
+		}
+
+	};
+
+	isInvalidCredentialsError(error: unknown): error is InvalidCredentialsError {
+		return error instanceof InvalidCredentialsError;
+	  }
+
+
+	logout = async (req: Request, res: Response) => {
+		const cookie : Cookie = req.session.cookie;
+		cookie.setExpires();
+		res.setCookie(
+			cookie
+		);
+		await res.send({
+			statusCode: StatusCode.Redirect,
+			message: "successful logout",
+			redirect: "/"
+		});
+	};
 }
---
+++ src/models/User.ts
@@ -0,0 +1,136 @@
+import postgres, { camel } from "postgres";
+import {
+	camelToSnake,
+	convertToCase,
+	createUTCDate,
+	snakeToCamel,
+} from "../utils";
+
+export interface UserProps {
+	id?: number;
+	email: string;
+	password: string;
+	createdAt: Date;
+	editedAt?: Date;
+	profile?: string;
+}
+
+export class DuplicateEmailError extends Error {
+	constructor() {
+		super("User with this email already exists.");
+	}
+}
+
+export class InvalidCredentialsError extends Error {
+	constructor() {
+		super("Invalid credentials.");
+	}
+}
+
+export default class User {
+	constructor(
+		private sql: postgres.Sql<any>,
+		public props: UserProps,
+	) {}
+
+	/**
+	 * TODO: Implement this method. It should insert a new
+	 * row into the "users" table with the provided props.
+	 */
+	static async create(
+		sql: postgres.Sql<any>,
+		props: UserProps,
+	): Promise<User> {
+		const connection = await sql.reserve();
+
+		props.createdAt = props.createdAt ?? createUTCDate();
+		
+		const [existingEmail] = await connection<UserProps[]>`
+			SELECT * FROM users 
+			WHERE email = ${props.email}
+			`;
+		if (!existingEmail)
+		{
+			const [row] = await connection<UserProps[]>`
+			INSERT INTO users
+				${sql(convertToCase(camelToSnake, props))}
+			RETURNING *
+			`;
+
+			await connection.release();
+
+			return new User(sql, convertToCase(snakeToCamel, row) as UserProps);
+		}
+		else
+		{
+			await connection.release();
+			throw new DuplicateEmailError();
+		}
+	}
+
+	/**
+	 * TODO: To "log in" a user, we need to check if the
+	 * provided email and password match an existing row
+	 * in the database. If they do, we return a new User instance.
+	 */
+	static async login(
+		sql: postgres.Sql<any>,
+		email: string,
+		password: string,
+	): Promise<User> {
+		const connection = await sql.reserve();
+		
+		const [userData] = await connection<UserProps[]>`
+		SELECT * FROM users WHERE LOWER(email) = ${email.toLowerCase()} AND password = ${password}`;
+		
+		if (userData)
+		{
+			await connection.release();
+			return new User(sql, convertToCase(snakeToCamel, userData) as UserProps);
+		}
+		else
+		{
+			await connection.release();
+			throw new InvalidCredentialsError();
+		}
+	}
+	static async read(
+		sql: postgres.Sql<any>,
+		id: number,
+	): Promise<User> 
+	{
+		const connection = await sql.reserve();
+		
+		const [userData] = await connection<UserProps[]>`
+		SELECT * FROM users WHERE id = ${id}`;
+		
+
+		await connection.release();
+		return new User(sql, convertToCase(snakeToCamel, userData) as UserProps);
+
+	}
+
+	async update(updateProps: Partial<UserProps>) {
+		const connection = await this.sql.reserve();
+
+		const [dupeEmail] = await connection<UserProps[]>`
+		SELECT * FROM users WHERE email = ${updateProps.email} and id != ${updateProps.id}`;
+		if (dupeEmail)
+		{
+			throw new InvalidCredentialsError();
+		}
+
+		const [row] = await connection`
+			UPDATE users
+			SET
+				${this.sql(convertToCase(camelToSnake, updateProps))}, edited_at = ${createUTCDate()}
+			WHERE
+				id = ${this.props.id}
+			RETURNING *
+		`;
+
+		await connection.release();
+
+		this.props = { ...this.props, ...convertToCase(snakeToCamel, row) };
+	}
+}
