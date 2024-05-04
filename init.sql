a681b1e3a7f0a0b3cf7ee7fa752cfeb80b5a93ac
Dylan Savelson <2281622@johnabbottcollege.net>
2024-05-04 00:24:41 +0000

    Added proper DB

--- init.sql
+++ init.sql
@@ -1,9 +1,27 @@
-DROP DATABASE IF EXISTS "MyDB";
-CREATE DATABASE "MyDB";
+DROP DATABASE IF EXISTS "MovieDB";
+CREATE DATABASE "MovieDB";
 
-\c MyDB;
+\c MovieDB;
 
-DROP TABLE IF EXISTS table_name;
-CREATE TABLE table_name (
+DROP TYPE IF EXISTS movie_status;
+CREATE TYPE movie_status AS ENUM ('watched', 'to_watch');
+
+DROP TABLE IF EXISTS movies;
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  status movie_status NOT NULL DEFAULT 'to_watch'
+  );
+
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+	  profile VARCHAR(255),
+	  is_admin BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    edited_at TIMESTAMP
 );
--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -7,6 +7,12 @@ import SessionManager from "../auth/SessionManager";
 import Cookie from "../auth/Cookie";
 import Session from "../auth/Session"
 import { spec } from "node:test/reporters";
+import {
+	camelToSnake,
+	convertToCase,
+	createUTCDate,
+	snakeToCamel,
+} from "../utils";
 
 
 /**
@@ -27,6 +33,9 @@ export default class AuthController {
 		router.get("/login", this.getLoginForm);
 		router.post("/login", this.login);
 		router.get("/logout", this.logout);
+		router.post("/users", this.createUser);
+		router.get("/users/:id/edit", this.getUserProfileForm)
+		router.post("/users/:id/edit", this.updateUserProfile)
 	}
 
 	getRegistrationForm = async (req: Request, res: Response) => {
@@ -136,6 +145,189 @@ export default class AuthController {
 			statusCode: StatusCode.Redirect,
 			message: "successful logout",
 			redirect: "/"
+		});
+	};
+	updateUserProfile = async (req: Request, res: Response) => {
+		if(req.session.get("userId"))
+		{
+			let user = await User.read(this.sql, req.session.get("userId"));
+			if (req.session.get("userId") == user.props.id)
+			{
+				const userProps: Partial<UserProps> = {
+					id: req.session.get("userId")
+				};
+
+				if (req.body.email) {
+					userProps.email = req.body.email;
+				}
+	
+				if (req.body.password) {
+					userProps.password = req.body.password;
+				}
+
+				if (req.body.profile) {
+					userProps.profile = req.body.profile;
+				}
+
+				if (req.body.darkmode)
+				{
+					res.setCookie(
+						new Cookie(
+							"theme",
+							"dark"
+						)
+					)
+				}
+				else
+				{
+					res.setCookie(
+						new Cookie(
+							"theme",
+							"light"
+						)
+					)
+				}
+
+				try 
+				{
+					await user.update(userProps);
+				} 
+				catch (error) 
+				{
+					await res.send({
+						statusCode: StatusCode.BadRequest,
+						message: "User with this email already exists",		
+						redirect: `/users/${user.props.id}/edit?failure=yes`,
+					});
+					return
+				}
+	
+				await res.send({
+					statusCode: StatusCode.OK,
+					message: "User updated successfully!",		
+					redirect: `/users/${user.props.id}/edit?success=yes`,
+				});
+			}
+		}
+		else
+		{
+			await res.send({
+				statusCode: StatusCode.Forbidden,
+				message: "not authenticated",
+				redirect: "/login",
+			});
+		}
+	}
+
+	getUserProfileForm = async (req: Request, res: Response) => {
+		const session = req.getSession();
+		res.setCookie( 
+			session.cookie
+		  );
+		if(req.session.get("userId"))
+		{
+			const user = await User.read(this.sql, req.session.get("userId"));
+			let darkMode: string = "";
+			if (req.findCookie("theme")?.value === "dark")
+			{
+				darkMode = "Dark mode enabled"	
+			}
+			else 
+			{
+				darkMode = "Dark mode disabled"
+			}
+			await res.send({
+				statusCode: StatusCode.OK,
+				message: "User Profile",
+				payload: {
+					isDarkMode: req.findCookie("theme")?.value === "dark",
+					darkModeMessage: darkMode,
+					message: req.getSearchParams().get("success") ?  "User updated successfully!" : "",
+					error: req.getSearchParams().get("failure") ?  "User with this email already exists" : "",
+					sessionCookie: session.get("userId") ? true : false,
+					userId: session.get("userId"),
+					email: user.props.email,
+					image: user.props.profile ? user.props.profile : "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+				},
+				template: "UserProfileView",
+			});
+		}
+		else
+		{
+			await res.send({
+				statusCode: StatusCode.Forbidden,
+				message: "not authenticated",
+				redirect: "/login",
+			});
+		}
+
+	};
+	/**
+	 * TODO: Upon form submission, this controller method should
+	 * validate that no fields are blank/missing, that the passwords
+	 * match, and that there isn't already a user with the given email.
+	 * If there are any errors, redirect back to the registration form
+	 * with an error message.
+	 * @param req
+	 * @param res
+	 */
+	createUser = async (req: Request, res: Response) => {
+		const password = req.body.password
+		const confirm_password = req.body.confirmPassword
+		const email = req.body.email
+
+		if (!email)
+		{
+			res.send({
+				statusCode: StatusCode.BadRequest,
+				message: "Email is required",
+				redirect: "/register?error=Email is required"
+			})
+			return
+		}
+
+		if (!password)
+		{
+			res.send({
+				statusCode: StatusCode.BadRequest,
+				message: "Password is required.",
+				redirect: "/register?error=Password is required"
+			})
+			return
+		}
+		if (password != confirm_password)
+		{
+			res.send({
+				statusCode: StatusCode.BadRequest,
+				message: "Passwords do not match",
+				redirect: "/register?error=Passwords do not match"
+			})
+			return
+		}
+
+		let user: User | null = null;
+
+		let userProps: UserProps = {
+			email: req.body.email,
+			password: req.body.password,
+			createdAt: createUTCDate(),
+		};
+
+		try {
+			user = await User.create(this.sql, userProps);
+		} catch (error) {
+			res.send({
+				statusCode: StatusCode.BadRequest,
+				message: "Invalid",
+				redirect: "/register"
+			})
+			return
+		}
+
+		await res.send({
+			statusCode: StatusCode.Created,
+			message: "User created successfully!",
+			redirect: `/login`,
 		});
 	};
 }
