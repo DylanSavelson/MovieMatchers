ef8f68d037ee44a675748a4bbf49d9511b5e4401
Rayan-Faccio <rayanfaccio@gmail.com>
2024-05-17 04:43:19 +0000

    added functinality to the methods in the watched and to watch models

--- src/models/ToWatchContent.ts
+++ src/models/ToWatchContent.ts
@@ -12,14 +12,27 @@ import Content from "./Content"
 export default class ToWatchContent{
     constructor(
         private sql: postgres.Sql<any>,
-        public toWatchList: [Content]
     ){}
 
-    static async add(sql: postgres.Sql<any>, content: Content){
+    static async add(sql: postgres.Sql<any>, content_id: number){
+        const connection = await sql.reserve();
+
+        await connection`
+            INSERT INTO to_watch_content
+            (content_id) VALUES (${content_id})
+        `;
         
+        connection.release();
     }
 
     static async remove(sql: postgres.Sql<any>, content_id: number){
+        const connection = await sql.reserve();
 
+        await connection`
+            DELETE FROM to_watch_list
+            WHERE content_id = ${content_id}
+        `;
+
+        await connection.release();
     }
 }
--- src/models/WatchedContent.ts
+++ src/models/WatchedContent.ts
@@ -12,18 +12,39 @@ import Content from "./Content"
 export default class ToWatchContent{
     constructor(
         private sql: postgres.Sql<any>,
-        public WatchedList: [Content]
     ){}
 
-    static async add(sql: postgres.Sql<any>, content: Content){
+    static async add(sql: postgres.Sql<any>, content_id: number){
+        const connection = await sql.reserve();
 
+        await connection`
+            INSERT INTO watched_content
+            (content_id) VALUES (${content_id})
+        `;
+        
+        connection.release();
     }
 
     static async update(sql: postgres.Sql<any>, content_id: number, new_rating: number){
+        const connection = await sql.reserve();
 
+        await connection`
+            UPDATE watched_content
+            SET (rating) = ${new_rating}
+            WHERE content_id = ${content_id}
+        `;
+
+        await connection.release();
     }
 
     static async remove(sql: postgres.Sql<any>, content_id: number){
+        const connection = await sql.reserve();
 
+        await connection`
+            DELETE FROM watched_list
+            WHERE content_id = ${content_id}
+        `;
+
+        await connection.release();
     }
 }
