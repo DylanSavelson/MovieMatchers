6ea13704cd9f820358d26d06b9340c1488c41a37
Rayan-Faccio <rayanfaccio@gmail.com>
2024-05-17 21:02:17 +0000

    Made changes to the user tests in the before each method.

--- tests/user.model.test.spec.ts
+++ tests/user.model.test.spec.ts
@@ -12,6 +12,47 @@ test.describe("CRUD operations", () => {
 
 	test.beforeEach(async () => {
 		// Anything you want to do before each test runs?
+		await sql.unsafe(`DROP TABLE IF EXISTS content CASCADE;
+		CREATE TABLE content (
+		  content_id SERIAL PRIMARY KEY,
+		  title VARCHAR(255) NOT NULL,
+		  description TEXT NOT NULL,
+		  content_poster TEXT NOT NULL,
+		  type content_type NOT NULL,
+			created_by TEXT[],
+			release_date TEXT,
+			genres TEXT[],
+			rating FLOAT,
+			seasons INTEGER
+		);
+		
+		
+		DROP TABLE IF EXISTS users CASCADE;
+		CREATE TABLE users (
+		  user_id SERIAL PRIMARY KEY,
+		  email VARCHAR(100) NOT NULL UNIQUE,
+		  password VARCHAR(255) NOT NULL,
+		  profile TEXT,
+		  is_admin BOOLEAN DEFAULT FALSE,
+		  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		  edited_at TIMESTAMP,
+		  visibility BOOLEAN DEFAULT TRUE
+		);
+		
+		DROP TABLE IF EXISTS watched_content CASCADE;
+		CREATE TABLE watched_content (
+		  user_id INTEGER REFERENCES users(user_id),
+		  content_id INTEGER REFERENCES content(content_id),
+		  PRIMARY KEY (user_id, content_id),
+		  rating FLOAT
+		);
+		
+		DROP TABLE IF EXISTS to_watch_content CASCADE;
+		CREATE TABLE to_watch_content (
+		  user_id INTEGER REFERENCES users(user_id),
+		  content_id INTEGER REFERENCES content(content_id),
+		  PRIMARY KEY (user_id, content_id)
+		);`);
 	});
 
 	/**
@@ -21,18 +62,6 @@ test.describe("CRUD operations", () => {
 	 */
 	test.afterEach(async () => {
 		// Replace the table_name with the name of the table(s) you want to clean up.
-		const tables = ["users", "content", "to_watch_content", "watched_content"];
-
-		try {
-			for (const table of tables) {
-				await sql.unsafe(`DELETE FROM ${table}`);
-				await sql.unsafe(
-					`ALTER SEQUENCE ${table}_id_seq RESTART WITH 1;`,
-				);
-			}
-		} catch (error) {
-			console.error(error);
-		}
 	});
 
 	// Close the connection to the DB after all tests are done.
