38d1f7d2ae95fa918f21db7551bc99a620d5fb03
Rayan-Faccio <rayanfaccio@gmail.com>
2024-05-17 19:26:38 +0000

    Made tests for user model

---
+++ tests/user.model.test.ts
@@ -0,0 +1,110 @@
+import postgres from "postgres";
+import {
+	test,
+	describe,
+	expect,
+	afterEach,
+	afterAll,
+	beforeEach,
+} from "vitest";
+import User from "../src/models/User"
+import { UserProps } from "../src/models/User"
+import { createUTCDate } from "../src/utils";
+
+describe("CRUD operations", () => {
+	// Set up the connection to the DB.
+	const sql = postgres({
+		database: "ContentDB",
+	});
+
+	beforeEach(async () => {
+		// Anything you want to do before each test runs?
+	});
+
+	/**
+	 * Clean up the database after each test. This function deletes all the rows
+	 * from the todos and subtodos tables and resets the sequence for each table.
+	 * @see https://www.postgresql.org/docs/13/sql-altersequence.html
+	 */
+	afterEach(async () => {
+		// Replace the table_name with the name of the table(s) you want to clean up.
+		const tables = ["users"];
+
+		try {
+			for (const table of tables) {
+				await sql.unsafe(`DELETE FROM ${table}`);
+				await sql.unsafe(
+					`ALTER SEQUENCE ${table}_id_seq RESTART WITH 1;`,
+				);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	});
+
+	// Close the connection to the DB after all tests are done.
+	afterAll(async () => {
+		await sql.end();
+	});
+
+	const createUser = async (props: Partial<UserProps> = {}) => {
+		return await User.create(sql, {
+			email: props.email || "user@email.com",
+			password: props.password || "password",
+			createdAt: props.createdAt || createUTCDate(),
+		});
+	};
+
+	test("User was created.", async () => {
+		const user = await createUser({ password: "Password123" });
+
+		expect(user.props.email).toBe("user@email.com");
+		expect(user.props.password).toBe("Password123");
+		expect(user.props.createdAt).toBeTruthy();
+		expect(user.props.editedAt).toBeFalsy();
+	});
+
+	test("User was not created with duplicate email.", async () => {
+		await createUser({ email: "user@email.com" });
+
+		await expect(async () => {
+			await createUser({ email: "user@email.com" });
+		}).rejects.toThrow("User with this email already exists.");
+	});
+
+	test("User was logged in.", async () => {
+		const user = await createUser({ password: "Password123" });
+		const loggedInUser = await User.login(
+			sql,
+			user.props.email,
+			"Password123",
+		);
+
+		expect(loggedInUser?.props.email).toBe("user@email.com");
+		expect(loggedInUser?.props.password).toBe("Password123");
+	});
+
+	test("User was not logged in with invalid password.", async () => {
+		const user = await createUser({ password: "Password123" });
+
+		await expect(async () => {
+			await User.login(sql, user.props.email, "wrongpassword");
+		}).rejects.toThrow("Invalid credentials.");
+	});
+
+	test("User was not logged in with invalid email.", async () => {
+		const user = await createUser({ password: "Password123" });
+
+		await expect(async () => {
+			await User.login(sql, "invalid@email.com", "password");
+		}).rejects.toThrow("Invalid credentials.");
+	});
+
+	test("User was not logged in with invalid email and password.", async () => {
+		const user = await createUser({ password: "Password123" });
+
+		await expect(async () => {
+			await User.login(sql, "invalid@email.com", "wrongpassword");
+		}).rejects.toThrow("Invalid credentials.");
+	});
+});
